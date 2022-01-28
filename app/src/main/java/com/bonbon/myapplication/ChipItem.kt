3d6f94e9269b170404ddfa2223a2f4b25dfd592b
bondatutu <bondatutu.angpututu@gmail.com>
2022-01-28 17:37:10 +1300

    Update coloring

--- app/src/main/java/com/bonbon/myapplication/ChipItem.kt
+++ app/src/main/java/com/bonbon/myapplication/ChipItem.kt
@@ -1,9 +1,12 @@
 package com.bonbon.myapplication
 
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import com.bonbon.library.FilterableEntity
 
 data class ChipItem(
-    val value: String = ""
+    val value: String = "",
+    val icon: Int? = null
 ) : FilterableEntity {
     override fun filter(query: String): Boolean {
         return value.startsWith(query, ignoreCase = true)
--- library/src/main/java/com/bonbon/library/ActionChip.kt
+++ library/src/main/java/com/bonbon/library/ActionChip.kt
@@ -1,15 +1,18 @@
 package com.bonbon.library
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
@@ -23,47 +26,13 @@ import androidx.compose.ui.unit.dp
 fun ActionChip(
     text: String,
     modifier: Modifier = Modifier,
-    avatar: ImageVector? = null,
-    color: Color = MaterialTheme.colors.primary,
-    shape: Shape = MaterialTheme.shapes.medium,
-    textPadding: PaddingValues = PaddingValues(4.dp),
-    textStyle: TextStyle = LocalTextStyle.current,
-    border: BorderStroke? = null,
-    elevation: Dp = 0.dp,
-    closeIcon: ImageVector,
-    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
-    onClose: () -> Unit
-) {
-    ActionChip(
-        text = text,
-        modifier = modifier,
-        avatar = if (avatar != null) rememberVectorPainter(avatar) else null,
-        elevation = elevation,
-        shape = shape,
-        color = color,
-        border = border,
-        closeIcon = rememberVectorPainter(closeIcon),
-        tint = tint,
-        textPadding = textPadding,
-        textStyle = textStyle
-    ) {
-        onClose()
-    }
-}
-
-@Composable
-fun ActionChip(
-    text: String,
-    modifier: Modifier = Modifier,
     avatar: Painter? = null,
     color: Color = MaterialTheme.colors.primary,
     shape: Shape = MaterialTheme.shapes.medium,
-    textPadding: PaddingValues = PaddingValues(4.dp),
     textStyle: TextStyle = LocalTextStyle.current,
     border: BorderStroke? = null,
     elevation: Dp = 0.dp,
     closeIcon: Painter,
-    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current),
     onClose: () -> Unit
 ) {
     Chip(
@@ -76,21 +45,31 @@ fun ActionChip(
             Row(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
+                if (avatar != null) {
+                    Icon(
+                        painter = avatar,
+                        contentDescription = "avatar",
+                        modifier = Modifier.size(24.dp)
+                            .padding(start = 4.dp)
+                            .border(1.dp, color, CircleShape)
+                    )
+                }
                 Text(
                     text = text,
                     style = textStyle,
-                    modifier = Modifier.padding(textPadding)
+                    modifier = Modifier.padding(8.dp, 4.dp)
                 )
 
                 Icon(
                     closeIcon,
                     contentDescription = "Close Chip",
                     modifier = Modifier
-                        .size(14.dp)
+                        .padding(end = 8.dp)
+                        .size(18.dp)
                         .clickable {
                             onClose()
                         },
-                    tint = tint
+
                 )
             }
         })
